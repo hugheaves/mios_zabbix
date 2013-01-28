bf90e0711f1e49d6e8dfecb08fdbaaf498c7d6b7
Hugh Eaves <hugh@hugheaves.com>
2013-01-28 10:32:21 -0500

    0.3 release

--- src/L_Zabbix_core.lua
+++ src/L_Zabbix_core.lua
@@ -26,7 +26,7 @@ local json = g_dkjson
 local log = g_log
 local util = g_util
 
-local PLUGIN_VERSION = "0.2"
+local PLUGIN_VERSION = "0.3"
 local LOG_PREFIX = "Zabbix"
 
 local CONFIG_FILE = "/tmp/zabbix_agentd.conf"
@@ -36,6 +36,7 @@ local LOG_PATH = "/var/log/cmh/"
 local SENDER_STDOUT_FILE = LOG_PATH .. "zabbix_sender.out"
 local AGENT_STDOUT_FILE = LOG_PATH .. "zabbix_agentd.out"
 local AGENT_LOG_FILE = LOG_PATH .. "zabbix_agentd.log"
+local SEND_LOG_FILE = LOG_PATH .. "zabbix_sendlog.txt"
 
 local BIN_PATH = "/etc/cmh-ludl/"
 local SENDER_BIN = "zabbix_sender"
@@ -63,6 +64,36 @@ local DEFAULT_LOG_CONFIG = {
 	}
 }
 
+-- Variable Types:
+-- value_type, data_type, units
+local TYPES = {
+	["urn:hugheaves-com:serviceId:SmartSwitchController1"] = {
+		["Timeout"] = { 0, 0, "unixtime" }
+	},
+	["urn:micasaverde-com:serviceId:HaDevice1"] = {
+		["BatteryDate"] = { 0, 0, "unixtime" },
+		["Configured"] = { 0, 0, "unixtime" },
+		["FirstConfigured"] = { 0, 0, "unixtime" },
+		["LastUpdate"] = { 0, 0, "unixtime" }
+	},
+	["urn:micasaverde-com:serviceId:SecuritySensor1"] = {
+		["LastTrip"] = { 0, 0, "unixtime" },
+		["Armed"] = { 3, 3, "" },
+		["Tripped"] = { 3, 3, "" }
+	},
+	["urn:micasaverde-com:serviceId:LightSensor1"] = {
+		["CurrentLevel"] = { 0, 0, "%%" }
+	},
+	["urn:upnp-org:serviceId:Dimming1"] ={
+		["LoadLevelStatus"] = { 0, 0, "%%" },
+		["LoadLevelTarget"] = { 0, 0, "%%" },
+	},
+	["urn:upnp-org:serviceId:SwitchPower1"] ={
+		["Status"] = { 3, 3, "" },
+		["Target"] = { 3, 3, "" }
+	}
+}
+
 -- GLOBALS
 local g_deviceId = nil
 local g_statusUrl = "http://localhost:3480/data_request?id=lu_status2&output_format=json"
@@ -82,11 +113,46 @@ local function executeCommand(command)
 	local returnCode = os.execute (command)
 	log.info ("Received return code [", returnCode, "] for command [", command, "]")
 end
+
 ------------------------------------------
 ---------- Zabbix Communication ----------
 ------------------------------------------
---- Callback that receives notification of any changes in UPnP variables
--- Collects the changes for periodic transmission by the sendEvents() function
+local function sendValue(deviceId, service, variable, value)
+	local hostNamePrefix = util.getLuupVariable(SID.ZABBIX, "HostNamePrefix", g_deviceId, util.T_STRING)
+
+	-- remove double quotes (if any) from value
+	local value = value:gsub("\"", "'");
+	
+	-- remove control characters (if any) from value
+	local value = value:gsub("%c", " ");
+	
+	local outputData = {
+		hostNamePrefix,
+		"_",
+		deviceId,
+		" ",
+		buildKey (service, variable),
+		" \"",
+		value,
+		"\"\n"
+	}
+
+	local outputLine = table.concat(outputData)
+
+	log.debug ("Sending to Zabbix, outputLine = |", outputLine, "|")
+--	log.debug ("g_sender type = ", io.type(g_sender))
+--	local sendLogFile = io.open(SEND_LOG_FILE, "a")
+--	sendLogFile:write (outputLine)
+--	sendLogFile:close ()
+
+	g_sender:write (outputLine)
+--	g_sender:flush ()
+	log.debug ("Done sending to Zabbix")
+end
+
+---------------------------------------------
+---------- Variable change callback ---------
+---------------------------------------------
 function variableWatchCallback(lul_device, lul_service, lul_variable, lul_value_old, lul_value_new)
 	log.debug("lul_device = ", lul_device,
 	", lul_service = ", lul_service,
@@ -94,27 +160,9 @@ function variableWatchCallback(lul_device, lul_service, lul_variable, lul_value_
 	", lul_value_old = ", lul_value_old,
 	", lul_value_new = ", lul_value_new)
 
-	local hostNamePrefix = util.getLuupVariable(SID.ZABBIX, "HostNamePrefix", g_deviceId, util.T_STRING)
-
-	local outputData = {
-		hostNamePrefix,
-		"_",
-		lul_device,
-		" ",
-		buildKey (lul_service, lul_variable),
-		" ",
-		lul_value_new,
-		"\n"
-	}
-
-	local outputLine = table.concat(outputData)
-
-	log.debug ("Sending to Zabbix, outputLine = |", outputLine, "|")
-
-	g_sender:write (outputLine)
-	g_sender:flush ()
-
-	log.debug ("Done sending to Zabbix")
+	if (lul_value_new ~= nil) then
+		sendValue(lul_device, lul_service, lul_variable, lul_value_new);
+	end
 
 end
 
@@ -127,22 +175,36 @@ local function generateTemplates(xmlOutput, statusData, templateType, templateGr
 
 	table.insert(xmlOutput, "<templates>\n")
 
-	-- build a list of services, variables, and variable types used by all currently active devices
+	-- build a list of variable types used by all currently active devices
 	local serviceList = {}
 	for i, deviceData in pairs (statusData.devices) do
 		for j, state in pairs(deviceData.states) do
 			if (serviceList[state.service] == nil) then
 				serviceList[state.service] = {}
 			end
-			-- assume any new value is numeric first
+
+			-- initialize the zabbix variable type to "number/float"
 			if (serviceList[state.service][state.variable] == nil) then
-				serviceList[state.service][state.variable] = 0
+				serviceList[state.service][state.variable] = { 0, 0, "" }
 			end
-			-- if we are unable to convert the value to a number, assume that
-			-- the variable holds character values
+
+			-- if we find something else other than a number value
+			-- in this variable, change the type to "character"
 			if (tonumber(state.value) == nil) then
-				serviceList[state.service][state.variable] = 1
+				serviceList[state.service][state.variable][1] = 1
 			end
+		end
+	end
+
+	log.debug ("Type Overrides = ", TYPES)
+	for service, variables in pairs(TYPES) do
+		log.debug ("Overriding variables in service ", service)
+		for variable, typeInfo in pairs(variables) do
+			log.debug ("Overriding variable type for service [", service, "], variable [", variable, "], typeInfo [", typeInfo, "]")
+			if (serviceList[service] == nil) then
+				serviceList[service] = {}
+			end
+			serviceList[service][variable] = typeInfo;
 		end
 	end
 
@@ -151,24 +213,25 @@ local function generateTemplates(xmlOutput, statusData, templateType, templateGr
 
 		xmlChunk =
 		[[
-        <template>
-            <template>__TEMPLATE_NAME__</template>
-            <name>__TEMPLATE_DISPLAY_NAME__</name>
-            <groups>
-                <group>
-                    <name>__TEMPLATE_GROUP_NAME__</name>
-                </group>
-            </groups>
-            <discovery_rules/>
-            <macros/>
-            <templates/>
-            <applications>
-            	<application>
-            		<name>__TEMPLATE_DISPLAY_NAME__</name>
-            	</application>
-            </applications>
-            <screens/>
-]]    
+<template>
+<template>__TEMPLATE_NAME__</template>
+<name>__TEMPLATE_DISPLAY_NAME__</name>
+<groups>
+<group>
+<name>__TEMPLATE_GROUP_NAME__</name>
+</group>
+</groups>
+<discovery_rules/>
+<macros/>
+<templates/>
+<applications>
+<application>
+<name>__TEMPLATE_DISPLAY_NAME__</name>
+</application>
+</applications>
+<screens/>
+]]
+
 		xmlChunk = xmlChunk:gsub("__TEMPLATE_GROUP_NAME__", templateGroupName)
 		xmlChunk = xmlChunk:gsub("__TEMPLATE_NAME__", service:gsub(":", "_"))
 		xmlChunk = xmlChunk:gsub("__TEMPLATE_DISPLAY_NAME__", service)
@@ -177,53 +240,54 @@ local function generateTemplates(xmlOutput, statusData, templateType, templateGr
 		table.insert(xmlOutput, "<items>\n")
 
 		if (templateType == "full") then
-
-			for variable, valueType in pairs(variables) do
+			for variable, typeInfo in pairs(variables) do
 				local xmlChunk =
 				[[
-                <item>
-                    <name>__VARIABLE__</name>
-                    <type>2</type>
-                    <snmp_community/>
-                    <multiplier>0</multiplier>
-                    <snmp_oid/>
-                    <key>__KEY__</key>
-                    <delay>0</delay>
-                    <history>90</history>
-                    <trends>365</trends>
-                    <status>0</status>
-                    <value_type>__VALUE_TYPE__</value_type>
-                    <allowed_hosts/>
-                    <units/>
-                    <delta>0</delta>
-                    <snmpv3_securityname/>
-                    <snmpv3_securitylevel>0</snmpv3_securitylevel>
-                    <snmpv3_authpassphrase/>
-                    <snmpv3_privpassphrase/>
-                    <formula>1</formula>
-                    <delay_flex/>
-                    <params/>
-                    <ipmi_sensor/>
-                    <data_type>0</data_type>
-                    <authtype>0</authtype>
-                    <username/>
-                    <password/>
-                    <publickey/>
-                    <privatekey/>
-                    <port/>
-                    <description/>
-                    <inventory_link>0</inventory_link>
-                    <valuemap/>
-            		<applications>
-            			<application>
-            				<name>__TEMPLATE_DISPLAY_NAME__</name>
-            			</application>
-            		</applications>
-			</item>
+<item>
+<name>__VARIABLE__</name>
+<type>2</type>
+<snmp_community/>
+<multiplier>0</multiplier>
+<snmp_oid/>
+<key>__KEY__</key>
+<delay>0</delay>
+<history>90</history>
+<trends>365</trends>
+<status>0</status>
+<value_type>__VALUE_TYPE__</value_type>
+<allowed_hosts/>
+<units>__UNITS__</units>
+<delta>0</delta>
+<snmpv3_securityname/>
+<snmpv3_securitylevel>0</snmpv3_securitylevel>
+<snmpv3_authpassphrase/>
+<snmpv3_privpassphrase/>
+<formula>1</formula>
+<delay_flex/>
+<params/>
+<ipmi_sensor/>
+<data_type>__DATA_TYPE__</data_type>
+<authtype>0</authtype>
+<username/>
+<password/>
+<publickey/>
+<privatekey/>
+<port/>
+<description/>
+<inventory_link>0</inventory_link>
+<valuemap/>
+<applications>
+<application>
+<name>__TEMPLATE_DISPLAY_NAME__</name>
+</application>
+</applications>
+</item>
 ]]
 				xmlChunk = xmlChunk:gsub("__VARIABLE__", variable)
 				xmlChunk = xmlChunk:gsub("__KEY__", buildKey(service,variable))
-				xmlChunk = xmlChunk:gsub("__VALUE_TYPE__", valueType)
+				xmlChunk = xmlChunk:gsub("__VALUE_TYPE__", typeInfo[1])
+				xmlChunk = xmlChunk:gsub("__DATA_TYPE__", typeInfo[2])
+				xmlChunk = xmlChunk:gsub("__UNITS__", typeInfo[3])
 				xmlChunk = xmlChunk:gsub("__TEMPLATE_DISPLAY_NAME__", service)
 				table.insert(xmlOutput, xmlChunk)
 
@@ -239,7 +303,7 @@ local function generateTemplates(xmlOutput, statusData, templateType, templateGr
 
 end
 
-local function generateHosts(xmlOutput, statusData, hostGroupName, hostNamePrefix)
+local function generateHosts(xmlOutput, statusData, hostGroupName, hostNamePrefix, agentHostName)
 	table.insert(xmlOutput, "<hosts>\n")
 
 	for i, deviceData in pairs (statusData.devices) do
@@ -247,39 +311,40 @@ local function generateHosts(xmlOutput, statusData, hostGroupName, hostNamePrefi
 
 		local xmlChunk =
 		[[
-			<host>
-            <host>__DEVICE_HOST_NAME_</host>
-            <name>__DEVICE_DISPLAY_NAME__</name>
-            <proxy/>
-            <status>0</status>
-            <ipmi_authtype>-1</ipmi_authtype>
-            <ipmi_privilege>2</ipmi_privilege>
-            <ipmi_username/>
-            <ipmi_password/>
-            <groups>
-                <group>
-                    <name>__HOST_GROUP_NAME__</name>
-                </group>
-            </groups>
-            <interfaces>
-                <interface>
-                    <default>1</default>
-                    <type>1</type>
-                    <useip>1</useip>
-                    <ip>127.0.0.1</ip>
-                    <dns/>
-                    <port>10050</port>
-                    <interface_ref>if1</interface_ref>
-                </interface>
-            </interfaces>
-            <applications/>
-            <items/>
-            <discovery_rules/>
-            <macros/>
-            <inventory/>
+<host>
+<host>__DEVICE_HOST_NAME_</host>
+<name>__DEVICE_DISPLAY_NAME__</name>
+<proxy/>
+<status>0</status>
+<ipmi_authtype>-1</ipmi_authtype>
+<ipmi_privilege>2</ipmi_privilege>
+<ipmi_username/>
+<ipmi_password/>
+<groups>
+<group>
+<name>__HOST_GROUP_NAME__</name>
+</group>
+</groups>
+<interfaces>
+<interface>
+<default>1</default>
+<type>1</type>
+<useip>0</useip>
+<ip></ip>
+<dns>__AGENT_HOST_NAME__</dns>
+<port>10050</port>
+<interface_ref>if1</interface_ref>
+</interface>
+</interfaces>
+<applications/>
+<items/>
+<discovery_rules/>
+<macros/>
+<inventory/>
 ]]
 
 		xmlChunk = xmlChunk:gsub("__HOST_GROUP_NAME__", hostGroupName)
+		xmlChunk = xmlChunk:gsub("__AGENT_HOST_NAME__", agentHostName)
 		xmlChunk = xmlChunk:gsub("__DEVICE_HOST_NAME_", hostNamePrefix .. "_" .. deviceId)
 		xmlChunk = xmlChunk:gsub("__DEVICE_DISPLAY_NAME__", hostNamePrefix .. " - " ..
 		luup.devices[deviceId].description .. " (#" .. deviceId .. ")")
@@ -305,15 +370,60 @@ local function generateHosts(xmlOutput, statusData, hostGroupName, hostNamePrefi
 	table.insert(xmlOutput, "</hosts>\n")
 end
 
+--
+-- Create Zabbix Header
+--
+local function generateHeader (xmlOutput, statusData, hostGroupName, templateGroupName)
+
+	local xmlChunk =
+	[[
+<?xml version="1.0" encoding="UTF-8"?>
+<zabbix_export>
+<version>2.0</version>
+<date>__DATE__</date>
+<groups>
+<group>
+<name>__HOST_GROUP_NAME__</name>
+</group>
+<group>
+<name>__TEMPLATE_GROUP_NAME__</name>
+</group>
+</groups>
+]]
+
+	xmlChunk = xmlChunk:gsub("__DATE__", os.date("%Y-%m-%dT%H:%M:%S", os.time()))
+	xmlChunk = xmlChunk:gsub("__HOST_GROUP_NAME__", hostGroupName)
+	xmlChunk = xmlChunk:gsub("__TEMPLATE_GROUP_NAME__", templateGroupName)
+	table.insert(xmlOutput, xmlChunk)
+
+end
+
+function generateZabbixHostFile  (lul_request, lul_parameters, lul_outputformat)
+	log.info ("Generating Zabbix host file, lul_request = ", lul_request, ", lul_parameters = ", lul_parameters, ", lul_outputformat = ", lul_outputformat)
+
+	local hostNamePrefix  = util.getLuupVariable(SID.ZABBIX, "HostNamePrefix", g_deviceId, util.T_STRING)
+	local hostGroupName = util.getLuupVariable(SID.ZABBIX, "HostGroupName", g_deviceId, util.T_STRING)
+	local templateGroupName = util.getLuupVariable(SID.ZABBIX, "TemplateGroupName", g_deviceId, util.T_STRING)
+	local agentHostName = util.getLuupVariable(SID.ZABBIX, "AgentHostName", g_deviceId, util.T_STRING)
+
+	local statusData = util.httpGetJSON(g_statusUrl)
+
+	local xmlOutput = {}
+
+	generateHeader(xmlOutput, statusData, hostGroupName, templateGroupName)
+	generateHosts(xmlOutput, statusData, hostGroupName, hostNamePrefix, agentHostName)
+
+	table.insert(xmlOutput, "</zabbix_export>\n")
+
+	log.info ("Done generating host file")
+
+	return table.concat(xmlOutput)
+
+end
+
 --- Generate a Zabbix XML host file that can be imported directly into Zabbix
-function generateZabbixImportFile (lul_request, lul_parameters, lul_outputformat)
-	log.info ("Generating Zabbix import file, lul_request = ", lul_request, ", lul_parameters = ", lul_parameters, ", lul_outputformat = ", lul_outputformat)
-
-	local templateType = "none"
-
-	if (lul_parameters.templates ~= nil) then
-		templateType = lul_parameters.templates
-	end
+function generateZabbixTemplateFile (lul_request, lul_parameters, lul_outputformat)
+	log.info ("Generating Zabbix template file, lul_request = ", lul_request, ", lul_parameters = ", lul_parameters, ", lul_outputformat = ", lul_outputformat)
 
 	local hostNamePrefix  = util.getLuupVariable(SID.ZABBIX, "HostNamePrefix", g_deviceId, util.T_STRING)
 	local hostGroupName = util.getLuupVariable(SID.ZABBIX, "HostGroupName", g_deviceId, util.T_STRING)
@@ -323,44 +433,13 @@ function generateZabbixImportFile (lul_request, lul_parameters, lul_outputformat
 
 	local xmlOutput = {}
 
-	--
-	-- Output Header
-	--
-
-	local xmlChunk =
-	[[
-<?xml version="1.0" encoding="UTF-8"?>
-<zabbix_export>
-    <version>2.0</version>
-    <date>__DATE__</date>
-    <groups>
-        <group>
-            <name>__HOST_GROUP_NAME__</name>
-        </group>
-		<group>
-			<name>__TEMPLATE_GROUP_NAME__</name>
-		</group>
-    </groups>
-]]
-
-	xmlChunk = xmlChunk:gsub("__DATE__", os.date("%Y-%m-%dT%H:%M:%S", os.time()))
-	xmlChunk = xmlChunk:gsub("__HOST_GROUP_NAME__", hostGroupName)
-	xmlChunk = xmlChunk:gsub("__TEMPLATE_GROUP_NAME__", templateGroupName)
-	table.insert(xmlOutput, xmlChunk)
-
-	--
-	-- Output Placeholder Template Definitions (in case they are not already defined in Zabbix)
-	--
-
-	if (templateType ~= "none") then
-		generateTemplates(xmlOutput, statusData, templateType, templateGroupName)
+	local templateType = "none"
+	if (lul_parameters.templates ~= nil) then
+		templateType = lul_parameters.templates
 	end
 
-	--
-	-- Output Host Configurations
-	--
-	generateHosts(xmlOutput, statusData, hostGroupName, hostNamePrefix)
-
+	generateHeader(xmlOutput, statusData, hostGroupName, templateGroupName)
+	generateTemplates(xmlOutput, statusData, templateType, templateGroupName)
 
 	table.insert(xmlOutput, "</zabbix_export>\n")
 
@@ -368,8 +447,6 @@ function generateZabbixImportFile (lul_request, lul_parameters, lul_outputformat
 
 	return table.concat(xmlOutput)
 end
-
-
 
 ------------------------------------------
 -------- Initialization Functions --------
@@ -384,9 +461,13 @@ local function registerListeners ()
 	-- for each Vera device ...
 	for i, deviceData in pairs(statusData.devices) do
 		local services = {}
-		-- build unique services list for each device
+		-- build unique services list for each device, and send initial values
 		for j, state in pairs(deviceData.states) do
 			services[state.service] = true
+			if (state.value == "") then
+				state.value = "-NONE-";
+			end
+			sendValue(deviceData.id, state.service, state.variable, state.value)
 		end
 		-- add listeners for each service in the list
 		for service, data in pairs(services) do
@@ -400,7 +481,7 @@ end
 --- init Luup variables if they don't have values
 local function initLuupVariables()
 	util.initVariableIfNotSet(SID.ZABBIX, "ZabbixServer", "0.0.0.0", g_deviceId)
-	util.initVariableIfNotSet(SID.ZABBIX, "AgentHostName", "vera", g_deviceId)
+	util.initVariableIfNotSet(SID.ZABBIX, "AgentHostName", "vera.localnet", g_deviceId)
 	util.initVariableIfNotSet(SID.ZABBIX, "HostNamePrefix", "vera", g_deviceId)
 	util.initVariableIfNotSet(SID.ZABBIX, "HostGroupName", "Vera Devices", g_deviceId)
 	util.initVariableIfNotSet(SID.ZABBIX, "TemplateGroupName", "MiOS Templates", g_deviceId)
@@ -409,10 +490,11 @@ end
 local function startZabbixSender()
 	executeCommand("chmod a+rx " .. SENDER_PATH)
 
-	local senderCommandLine = SENDER_PATH .. " -c " .. CONFIG_FILE .. " -r -i - 1>" .. SENDER_STDOUT_FILE .. " 2>&1"
+	local senderCommandLine = SENDER_PATH .. " -vv -c " .. CONFIG_FILE .. " -r -i - 1>" .. SENDER_STDOUT_FILE .. " 2>&1"
 	log.info ("senderCommandLine = ", senderCommandLine)
 
 	g_sender = io.popen(senderCommandLine, "w")
+	g_sender:setvbuf("no")
 	g_sender:flush ()
 end
 
@@ -423,7 +505,7 @@ local function startZabbixAgent()
 	-- Even after the kill command has executed, we need to sleep
 	-- a little more to give enough time for all zabbix_agentd
 	-- subprocesses to stop
-	luup.sleep(2000)
+	luup.sleep(5000)
 
 	local configFile = io.open(CONFIG_FILE, "w")
 	configFile:write ("AllowRoot=1\n")
@@ -437,7 +519,7 @@ local function startZabbixAgent()
 	executeCommand(AGENT_PATH .. " -c " .. CONFIG_FILE .. " 1>" .. AGENT_STDOUT_FILE .. " 2>&1 &")
 end
 
---- Initialize the  plugin
+--- Initialize the plugin
 function initialize(lul_device)
 	local success = false
 	local errorMsg = nil
@@ -459,9 +541,8 @@ function initialize(lul_device)
 
 	registerListeners()
 
-	luup.register_handler("generateZabbixImportFile","generateZabbixImportFile")
-
-	log.info("Done with initialization")
+	luup.register_handler("generateZabbixHostFile","zabbixHostFile.xml")
+	luup.register_handler("generateZabbixTemplateFile","zabbixTemplateFile.xml")
 
 	return success, errorMsg, "Zabbix"
 end
